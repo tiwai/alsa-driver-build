27fb94a75f104e6897d40505211da7bf127ce04b
Takashi Iwai <tiwai@suse.de>
2013-09-26 16:18:46 +0200

    Fix build failures due to movement of tea575x to media tree
    In kernel 3.12, tea575x tuner stuff was moved to media directory, so
    we are no longer keeping this stuff in our tree.  Build it
    conditionally only with the new kernels.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- alsa/pci/es1968.c
+++ alsa/pci/es1968.c
@@ -1,12 +1,3 @@
 #include "adriver.h"
-/* dummy v4l2_device definition */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 29)
-struct v4l2_device {
-	int dummy;
-};
-#define v4l2_file_operations	file_operations
-#define v4l2_device_register(x, y) 0
-#define v4l2_device_unregister(x)
-#endif
 #include "../alsa-kernel/pci/es1968.c"
 EXPORT_NO_SYMBOLS;
